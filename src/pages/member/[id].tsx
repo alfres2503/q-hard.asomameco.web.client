d2b427495b262046159c8810057302d93653cc72
Alfredo Suárez <94250490+alfres2503@users.noreply.github.com>
2024-02-10 18:15:27 -0600

    fix: api infrastructure

---
+++ src/pages/member/[id].tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MemberDetails = () => {
+  // Fetch member details based on memberId and store it in a state variable
+
+  // Render member details
+  return (
+    <div>
+      <h1>Member Details</h1>
+      <p>Member ID:</p>
+      {/* Display other member details here */}
+    </div>
+  );
+};
+
+export default MemberDetails;
